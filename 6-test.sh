146628a6bbd064318fe41ffb0d4c5d4947f613ac
Supraja <supra556699@gmail.com>
2024-11-24 23:18:13 +0800

    test

---
+++ 6-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "please enter your username::"
+read -s USERNAME #takes input into USERNAME variable
+echo "username entered is :$USERNAME"
+
+echo "please enter your password::"
+read -s PASSWORD #takes input into USERNAME variable
+echo "password entered is :$PASSWORD"
