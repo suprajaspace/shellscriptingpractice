bf24702ab08af8ccaf58d2fddda01c4d06a8e48a
Supraja <supra556699@gmail.com>
2024-11-24 23:07:00 +0800

    test

---
+++ 4-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Kanna
+PERSON2=Nikki
+echo "$PERSON1:: Hi ${PERSON2}, How r u?"
+echo "${PERSON2}:: hello $PERSON1.Iam fine,how r h doing?"
+echo "$PERSON1::I am doing good. ${PERSON2}  what's going on?"
+echo "${PERSON2}::I started Learning shell scripting $PERSON1"
